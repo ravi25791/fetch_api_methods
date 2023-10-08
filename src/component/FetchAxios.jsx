82a76f6e7c2802995be8578a815aace39ce883aa
Ravi Kumar <ravi.kumar4rawat@gmail.com>
2023-10-08 15:19:47 +0530

    hello

--- src/component/FetchAxios.jsx
+++ src/component/FetchAxios.jsx
@@ -3,29 +3,33 @@ import axios from 'axios'
 
 const FetchAxios = () => {
     const [myData, setMyData] = useState([])
+    const [error,setError] =useState("")
+
+
 
     useEffect(() => {
-        axios.get("https://jsonplaceholder.typicode.com/users")
-            .then((response) => setMyData(response))
+        axios.get("https://jsonplaceholder.typicode.com/posts")
+            .then((response) => setMyData(response.data))
+            .catch((err)=>{setError(err.message)})
 
     }, [])
 
     return (
         <div>
             <h1>This is Fetch Method With Axios</h1>
+            <h2>{error==! "" && {error}}</h2>
             <div className="conatiner">
                 <div className="row">
                     {
                         myData.map((items) => {
-                            const { name, username, email, id } = items;
+                            const { userId, title, body, id } = items;
                             return (
                                 <div className="col-md-4" key={id}>
                                     <div className="card" style={{ width: "18rem", margin: "10px" }}>
                                         <div className="card-body">
-                                            <h5 className="card-title">{name}</h5>
-                                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                            <p>{username}</p>
-                                            <p>{email}</p>
+                                            <h5 className="card-title">{title}</h5>
+                                            <p className="card-text">{body}</p>
+                                            <p>{userId}</p>
                                             <a href="#" className="btn btn-primary">Go somewhere</a>
                                         </div>
                                     </div>
